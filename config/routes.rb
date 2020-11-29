dde43bda3cf797c05667dccc0a7b75466fb37cff
U-CLOUD9 <szkyt0x9a@gmail.com>
2020-11-29 09:05:43 +0000

    +AccountCreate,+LogIn/Out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#index'
+  
+  get  'sessions/new'
   get  'pages/help'
+  
+  resources :users
+  
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
