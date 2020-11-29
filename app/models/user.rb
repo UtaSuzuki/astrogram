b8a91e1fd107c0bc23006e0f2671fc385294d552
U-CLOUD9 <szkyt0x9a@gmail.com>
2020-11-29 14:48:37 +0000

    +LogoIcon

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 15}
   validates :email, presence: true, uniqueness: true, format: {with: /\A[\w\-\.]+@[a-zA-Z\d\-]+\.[a-zA-Z]+\z/}
   validates :password, confirmation: true, format: {with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{8,32}\z/}
+  validates :kind, inclusion: {in: %w(normal creater)}
   
   has_secure_password
-  
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_082407) do
+ActiveRecord::Schema.define(version: 2020_11_29_062514) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
+    t.string "kind"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
   end
 
 end
