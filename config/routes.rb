058f7c46f2a85b1880562a89e156bbfec469c488
U-CLOUD9 <szkyt0x9a@gmail.com>
2020-12-02 13:40:57 +0000

    complete login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get  'sessions/new'
   get  'pages/help'
   
-  resources :users
-  
   get    '/login',  to: 'sessions#new'
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
+  resources :users
 end
